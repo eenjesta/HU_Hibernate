9bacd0293bee9f12943c8dc0b20c3e232d654acd
Alex Eenjes <alexeenjes@alexs-mbp.wifi.hu.nl>
2021-10-11 13:55:59 +0200

    first commit

---
+++ src/OVChipkaart.java
@@ -0,0 +1,103 @@
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Table(name = "ov_chipkaart")
+public class OVChipkaart {
+     @Id
+     @Column(name = "kaart_nummer")
+     private int kaart_nummer;
+     private LocalDate geldig_tot;
+     private int klasse;
+     private double saldo;
+
+     @ManyToOne
+     @JoinColumn(name="reiziger_id", nullable=false)
+     private Reiziger reiziger;
+
+     @ManyToMany(mappedBy = "ov_chipkaarten")
+     private List<Product> producten;
+
+     public OVChipkaart(){};
+
+     public OVChipkaart(int kaart_nummer, LocalDate geldig_tot, int klasse, double saldo, Reiziger reiziger) {
+          this.kaart_nummer = kaart_nummer;
+          this.geldig_tot = geldig_tot;
+          this.klasse = klasse;
+          this.saldo = saldo;
+          this.reiziger = reiziger;
+     }
+
+     public int getKaart_nummer() {
+          return kaart_nummer;
+     }
+
+     public void setKaart_nummer(int kaart_nummer) {
+          this.kaart_nummer = kaart_nummer;
+     }
+
+     public LocalDate getGeldig_tot() {
+          return geldig_tot;
+     }
+
+     public void setGeldig_tot(LocalDate geldig_tot) {
+          this.geldig_tot = geldig_tot;
+     }
+
+     public int getKlasse() {
+          return klasse;
+     }
+
+     public void setKlasse(int klasse) {
+          this.klasse = klasse;
+     }
+
+     public double getSaldo() {
+          return saldo;
+     }
+
+     public void setSaldo(double saldo) {
+          this.saldo = saldo;
+     }
+
+     public Reiziger getReiziger() {
+          return reiziger;
+     }
+
+     public void setReiziger(Reiziger reiziger) {
+          this.reiziger = reiziger;
+     }
+
+     public List<Product> getProducten() {
+          return producten;
+     }
+
+     public void setProducten(List<Product> producten) {
+          this.producten = producten;
+     }
+
+     @Override
+     public boolean equals(Object obj) {
+          boolean sameObjects = false;
+          if (obj instanceof OVChipkaart) {
+               OVChipkaart otherOV_chipkaart = (OVChipkaart) obj;
+
+               if (this.kaart_nummer == otherOV_chipkaart.kaart_nummer && this.reiziger.getReiziger_id().equals(reiziger.getReiziger_id())) {
+                    sameObjects = true;
+               }
+          }
+          return sameObjects;
+     }
+
+     @Override
+     public int hashCode() {
+          return Objects.hash(kaart_nummer, geldig_tot, klasse, saldo, reiziger);
+     }
+
+     @Override
+     public String toString() {
+          return String.format("#%d: kaartnummer: %d, geldig tot: %s, klasse: %d, saldo: %,.2f", this.reiziger.getReiziger_id(), this.kaart_nummer, this.geldig_tot, this.klasse, this.saldo);
+     }
+}
---
+++ src/Reiziger.java
@@ -0,0 +1,139 @@
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Reiziger {
+    @Id
+    @Column(name = "reiziger_id")
+    private int reiziger_id;
+    private String voorletters;
+    private String tussenvoegsel;
+    private String achternaam;
+    private LocalDate geboortedatum;
+
+    @OneToOne(mappedBy = "reiziger")
+    private Adres adres;
+
+    @OneToMany(mappedBy="reiziger")
+    private List<OVChipkaart> ov_chipkaarten = new ArrayList<>();
+
+    public Reiziger(){};
+
+    public Reiziger(int reiziger_id, String voorletters, String tussenvoegsel, String achternaam, LocalDate geboortedatum) {
+        this.reiziger_id = reiziger_id;
+        this.voorletters = voorletters;
+        this.tussenvoegsel = tussenvoegsel;
+        this.achternaam = achternaam;
+        this.geboortedatum = geboortedatum;
+    }
+
+    public Integer getReiziger_id() {
+        return reiziger_id;
+    }
+
+    public void setReiziger_id(int reiziger_id) {
+        this.reiziger_id = reiziger_id;
+    }
+
+    public String getVoorletters() {
+        return voorletters;
+    }
+
+    public void setVoorletters(String voorletters) {
+        this.voorletters = voorletters;
+    }
+
+    public String getTussenvoegsel() {
+        return tussenvoegsel;
+    }
+
+    public void setTussenvoegsel(String tussenvoegsel) {
+        this.tussenvoegsel = tussenvoegsel;
+    }
+
+    public String getAchternaam() {
+        return achternaam;
+    }
+
+    public void setAchternaam(String achternaam) {
+        this.achternaam = achternaam;
+    }
+
+    public LocalDate getGeboortedatum() {
+        return geboortedatum;
+    }
+
+    public void setGeboortedatum(LocalDate geboortedatum) {
+        this.geboortedatum = geboortedatum;
+    }
+
+    public Adres getAdres() {
+        return adres;
+    }
+
+    public void setAdres(Adres adres) {
+        this.adres = adres;
+    }
+
+    public List<OVChipkaart> getOv_chipkaarten() {
+        return ov_chipkaarten;
+    }
+
+    public void setOv_chipkaarten(List<OVChipkaart> ov_chipkaarten) {
+        this.ov_chipkaarten = ov_chipkaarten;
+    }
+
+    public void addOv_chipkaart(OVChipkaart ov_chipkaart) {
+        List<OVChipkaart> toAdd = new ArrayList<>();
+        if (this.ov_chipkaarten.isEmpty()) {
+            this.ov_chipkaarten.add(ov_chipkaart);
+        } else {
+            for (OVChipkaart ov_chipkaart_list : this.ov_chipkaarten) {
+                if (ov_chipkaart.equals(ov_chipkaart_list)) {
+                    System.out.print("Oops deze OV-kaart bestaat al!");
+                } else {
+                    toAdd.add(ov_chipkaart);
+                }
+            }
+            this.ov_chipkaarten.addAll(toAdd);
+        }
+    }
+
+    public void updateOv_chipkaart(OVChipkaart ov_chipkaart) {
+        if (this.ov_chipkaarten.isEmpty()) {
+            System.out.print("Oops deze OV-kaart bestaat niet!");
+        } else {
+            for (OVChipkaart ov_chipkaart_list : this.ov_chipkaarten) {
+                if (ov_chipkaart.equals(ov_chipkaart_list)) {
+                    int index = this.ov_chipkaarten.indexOf(ov_chipkaart_list);
+                    this.ov_chipkaarten.set(index, ov_chipkaart);
+                }
+            }
+        }
+    }
+
+    public void deleteOv_chipkaart(OVChipkaart ov_chipkaart) {
+        List<OVChipkaart> toRemove = new ArrayList<>();
+        if (this.ov_chipkaarten.isEmpty()) {
+            System.out.print("Oops deze OV-kaart bestaat niet!");
+        } else {
+            for (OVChipkaart ov_chipkaart_list : this.ov_chipkaarten) {
+                if (ov_chipkaart.equals(ov_chipkaart_list)) {
+                    toRemove.add(ov_chipkaart);
+                }
+            }
+            this.ov_chipkaarten.removeAll(toRemove);
+        }
+    }
+
+    @Override
+    public String toString() {
+        if (this.tussenvoegsel != null) {
+            return String.format("Reiziger: {#%d %s %s %s, geb. %s, Adres {%s}, OV-chipkaarten {%s}}", this.reiziger_id, this.voorletters, this.tussenvoegsel, this.achternaam, this.geboortedatum, this.adres, this.ov_chipkaarten);
+        } else {
+            return String.format("Reiziger: {#%d %s %s, geb. %s, Adres {%s}, OV-chipkaarten {%s}}", this.reiziger_id, this.voorletters, this.achternaam, this.geboortedatum, this.adres, this.ov_chipkaarten);
+        }
+    }
+}
